b0de9d78c7171852af07770d0abf62fc2962c558
xiaomisu7 <dio8ssx@outlook.com>
2024-06-17 15:36:35 +0800

    Modify home.cpp

--- home.cpp
+++ home.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Senior PM leave site writer town.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
