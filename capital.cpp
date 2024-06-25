9ff5ea676b272e5792697b251884069502de2777
xiaomisu7 <dio8ssx@outlook.com>
2024-06-25 23:17:32 +0800

    Modify capital.cpp

--- capital.cpp
+++ capital.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Attention force town morning job.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
