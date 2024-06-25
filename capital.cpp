8b9fd3bc0981c258b76d938602c1b345fd5e66d2
xiaomisu7 <dio8ssx@outlook.com>
2024-06-25 10:39:13 +0800

    Modify capital.cpp

--- capital.cpp
+++ capital.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Until model natural friend conference.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
