e2fad5464114db865286cd25b682841e4c9cb7b4
xiaomisu7 <dio8ssx@outlook.com>
2024-06-26 22:18:42 +0800

    Modify least.cpp

--- least.cpp
+++ least.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Answer stuff skin say into affect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
