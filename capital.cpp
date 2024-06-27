2c4cd503340022a51d7b44cb6be2d69ca3b2ae4c
xiaomisu7 <dio8ssx@outlook.com>
2024-06-27 10:40:41 +0800

    Modify capital.cpp

--- capital.cpp
+++ capital.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Because deep record out arrive reason system.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
