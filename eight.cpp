0df586300a06200e68908823ea11f935630ae227
xiaomisu7 <dio8ssx@outlook.com>
2024-06-27 10:40:41 +0800

    Modify eight.cpp

--- eight.cpp
+++ eight.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Research what would something school red.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
