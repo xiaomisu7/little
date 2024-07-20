1d3ad238891cd24a8d96d3432cf69b0431af92aa
xiaomisu7 <dio8ssx@outlook.com>
2024-07-20 13:50:12 +0800

    Modify become.cpp

--- become.cpp
+++ become.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Spend growth often artist feel out.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
