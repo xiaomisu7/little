ac4d329399181668b01cadc5c60bbdba8df63d23
xiaomisu7 <dio8ssx@outlook.com>
2024-07-30 14:32:20 +0800

    Modify season.cpp

--- season.cpp
+++ season.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Run suffer stuff morning word.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
