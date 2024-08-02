bea93759e5c42a7f7c663ee24fdb03fdc55f1dc4
xiaomisu7 <dio8ssx@outlook.com>
2024-08-02 14:58:39 +0800

    Modify season.cpp

--- season.cpp
+++ season.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Board film seek thank now never.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
