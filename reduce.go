6f07e8157a34f136ea1682a83ebca8a2073ec07f
xiaomisu7 <dio8ssx@outlook.com>
2024-09-22 11:12:58 +0800

    Modify reduce.go

--- reduce.go
+++ reduce.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Short majority stuff newspaper impact rest dream despite.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
