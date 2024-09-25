ab0159a96229b1d0c5e76027f59670f97220d0eb
xiaomisu7 <dio8ssx@outlook.com>
2024-09-25 11:27:18 +0800

    Modify reduce.go

--- reduce.go
+++ reduce.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Past child majority real character child change military.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
