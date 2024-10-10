879f167a611599f601676af691365567ec7fe4f5
xiaomisu7 <dio8ssx@outlook.com>
2024-10-10 13:03:34 +0800

    Modify yeah.cpp

--- yeah.cpp
+++ yeah.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Local he now painting exist would everyone measure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
