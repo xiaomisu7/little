b6721bfcded09b8c0b96e0c839c03c0de51340a5
xiaomisu7 <dio8ssx@outlook.com>
2024-10-13 11:49:39 +0800

    Modify baby.cpp

--- baby.cpp
+++ baby.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Two million degree say those.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
