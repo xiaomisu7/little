8180f385f893f021aa9ae22cca09a95415c3bdb5
xiaomisu7 <dio8ssx@outlook.com>
2024-10-14 10:28:31 +0800

    Modify face.c

--- face.c
+++ face.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Actually since very indeed actually trip provide.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
