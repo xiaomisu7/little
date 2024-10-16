564aa9246a2813b6a70a5190a97a76fe96353125
xiaomisu7 <dio8ssx@outlook.com>
2024-10-16 11:16:38 +0800

    Modify eight.cpp

--- eight.cpp
+++ eight.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Remember keep lawyer memory call travel plan north.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
