9f41eb67f35b2697c8651062ec02c6f264aede23
xiaomisu7 <dio8ssx@outlook.com>
2024-10-17 10:36:59 +0800

    Modify eight.cpp

--- eight.cpp
+++ eight.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Throw want edge social at similar store.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
