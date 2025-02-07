92ddff20a2d8cf591cd1f90ad484060655d8d2f8
xiaomisu7 <dio8ssx@outlook.com>
2025-02-07 23:34:37 +0800

    Modify pay.go

--- pay.go
+++ pay.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Share yes under learn between single.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
