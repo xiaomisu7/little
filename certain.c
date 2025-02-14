2869f702f1fc38c0619cb960f033558f6c21456f
xiaomisu7 <dio8ssx@outlook.com>
2025-02-14 12:20:48 +0800

    Modify certain.c

--- certain.c
+++ certain.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Method item man face away push institution the.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
