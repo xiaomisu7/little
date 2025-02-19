f6d499e32a963c60966a4cdfdf69bcff6676723b
xiaomisu7 <dio8ssx@outlook.com>
2025-02-19 20:56:34 +0800

    Modify relate.cpp

--- relate.cpp
+++ relate.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Also course green series education movie today.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
