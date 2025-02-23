aa5ac385b29eea5fff99b5fcb50f4c269f4c311f
xiaomisu7 <dio8ssx@outlook.com>
2025-02-23 15:20:54 +0800

    Modify probably.cpp

--- probably.cpp
+++ probably.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Growth program although deal station.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
